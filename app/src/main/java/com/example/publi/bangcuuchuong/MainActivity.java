0cb81868072619e1fbdd11f5d84804a15ae195f8
nnititnn <hoanglongemson@gmail.com>
2015-11-20 11:15:00 +0700

    Them chu thich

--- app/src/main/java/com/example/publi/bangcuuchuong/MainActivity.java
+++ app/src/main/java/com/example/publi/bangcuuchuong/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     TextView ketqua;
     EditText a;
 
+    //DEmodfghjklghyuyuykkkjkjgjkgktutuitichodNHCHODANH
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
